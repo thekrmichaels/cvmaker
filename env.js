671bd524584b4194981b0bdae3df7c96c0378f0c
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-01-13 12:09:54 -0500

    build: 🛂 install and use Supabase

--- env.js
+++ env.js
@@ -1,8 +1,9 @@
-const { VITE_CLIENT_ID, VITE_DOMAIN, VITE_CVMAKER_API } = import.meta.env;
+const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY, VITE_CVMAKER_API } =
+  import.meta.env;
 
-const Auth0 = {
-  clientId: VITE_CLIENT_ID,
-  domain: VITE_DOMAIN,
+const Supabase = {
+  supabaseUrl: VITE_SUPABASE_URL,
+  supabaseKey: VITE_SUPABASE_ANON_KEY,
 };
 
-export { Auth0, VITE_CVMAKER_API as CVMaker_API };
+export { Supabase, VITE_CVMAKER_API as CVMaker_API };
