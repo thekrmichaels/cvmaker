9e98775f7ded648de8eeb466e37e70dd0c56c94d
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-14 20:07:53 -0500

    feat: 💄 add light/dark mode globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
 import { PrimeReactProvider } from "primereact/api";
+import { ThemeProvider } from "./contexts/ThemeProvider.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -9,7 +10,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <PrimeReactProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </PrimeReactProvider>
     </AuthProvider>
   </StrictMode>,
