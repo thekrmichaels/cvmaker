2fd2eb4a87ec342dc37a5b757b7ff6bb16fe6cc1
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-16 20:48:05 -0500

    feat: 💄 add main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { lazy } from "react";
+import { Link } from "react-router";
+import { useAuthContext, useThemeContext } from "./hooks/useContexts";
+import GoogleSignInButton from "./components/Google/GoogleSignInButton";
+import Loader from "./components/Suspense/Loader";
+import Suspense from "./components/Suspense/Suspense";
 
-function App() {
-  const [count, setCount] = useState(0)
+const UserDropdown = lazy(
+  () => import("./components/UserDropdown/UserDropdown"),
+);
+
+function App({ children }) {
+  const { isAuthenticated, isLoading, user } = useAuthContext();
+  const { isLightMode, bgClass, textClass } = useThemeContext();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main
+      className={`bg-pattern bg-pattern--${isLightMode ? "light" : "dark"} relative flex min-h-screen w-full flex-col overflow-hidden`}
+    >
+      <NavBar {...{ isLightMode, bgClass, textClass, isAuthenticated, user }} />
+
+      <div className="flex-1">
+        {isLoading ? <Loader loadText="Authenticating" /> : children}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {!isLoading && <Footer {...{ bgClass, textClass }} />}
+    </main>
+  );
 }
 
-export default App
+const Footer = ({ bgClass, textClass }) => (
+  <footer
+    className={`${bgClass} ${textClass} w-full px-4 py-2 text-start text-sm font-normal shadow-[0_-1px_2px_0_#747775] dark:shadow-[0_-1px_2px_0_#8E918F]`}
+  >
+    <p>
+      <strong>Dev:</strong>{" "}
+      <a
+        href="https://github.com/thekrmichaels"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline"
+      >
+        Amilkar Díaz
+      </a>
+    </p>
+    <p>
+      <strong>Support:</strong>{" "}
+      <a href="mailto:amilkarjdiazh@gmail.com" className="underline">
+        amilkarjdiazh@gmail.com
+      </a>
+    </p>
+  </footer>
+);
+
+const NavBar = ({ isLightMode, bgClass, textClass, isAuthenticated, user }) => (
+  <nav
+    className={`${bgClass} ${textClass} fixed top-0 z-10 flex w-full items-center justify-between px-4 py-2 shadow-sm shadow-[#747775] dark:shadow-[#8E918F]`}
+    aria-label="Navigation bar"
+  >
+    <div className="flex-1">
+      <Link to="/" className="flex items-center gap-2">
+        <img
+          src={`${import.meta.env.BASE_URL}/cvmaker-${isLightMode ? "light" : "dark"}.svg`}
+          alt="CVMaker icon"
+          className="h-[1.375rem] w-[1.0625rem]"
+        />
+        <h1 className="text-sm font-bold">CVMaker</h1>
+      </Link>
+    </div>
+    <Link to="/pricing" className="flex-1 text-center text-sm font-semibold">
+      Pricing
+    </Link>
+    <div className="flex-1 text-right">
+      {isAuthenticated ? (
+        <Suspense>
+          <UserDropdown user={user} />
+        </Suspense>
+      ) : (
+        <GoogleSignInButton />
+      )}
+    </div>
+  </nav>
+);
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { PrimeReactProvider } from "primereact/api";
 import { ThemeProvider } from "./contexts/ThemeProvider.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./routes/Home.jsx";
+import Pricing from "./routes/Pricing.jsx";
+
+const BASE_PATH = "/cvmaker";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <PrimeReactProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </PrimeReactProvider>
+      <BrowserRouter basename={BASE_PATH}>
+        <PrimeReactProvider>
+          <ThemeProvider>
+            <App>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/pricing" element={<Pricing />} />
+              </Routes>
+            </App>
+          </ThemeProvider>
+        </PrimeReactProvider>
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 );
