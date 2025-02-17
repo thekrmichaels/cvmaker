7e7c085b569ca5a10b1a4b76c3bff6222f7b8edb
Amilkar Díaz <amilkarjdiazh@outlook.com>
2025-02-16 20:48:05 -0500

    feat: 💄 add main page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Auth0 } from "../env.js";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { PrimeReactProvider } from "primereact/api";
 import { ThemeProvider } from "./contexts/ThemeProvider.jsx";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./routes/Home.jsx";
+import Pricing from "./routes/Pricing.jsx";
 
 const { clientId, domain } = Auth0;
+const BASE_PATH = "/cvmaker";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,15 +20,22 @@ createRoot(document.getElementById("root")).render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: window.location.origin + "/cvmaker",
+        redirect_uri: window.location.origin + BASE_PATH,
       }}
     >
       <AuthProvider>
-        <PrimeReactProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </PrimeReactProvider>
+        <BrowserRouter basename={BASE_PATH}>
+          <PrimeReactProvider>
+            <ThemeProvider>
+              <App>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/pricing" element={<Pricing />} />
+                </Routes>
+              </App>
+            </ThemeProvider>
+          </PrimeReactProvider>
+        </BrowserRouter>
       </AuthProvider>
     </Auth0Provider>
   </StrictMode>,
